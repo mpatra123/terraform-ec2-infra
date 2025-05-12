f4b427c9876eb075dcc27ed630ec861d31b24f35
mpatra123 <monicapatra@hotmail.com>
2025-05-12 16:38:37 +0800

    Terraform EC2 infra with variables and data sources

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "public" {
   ami                         = "ami-0e8ebb0ab254bb563" # find the AMI ID of Amazon Linux 2023  
   instance_type = "t2.micro"
-  subnet_id                   = "subnet-051ee0a63645aba11"  #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
+  subnet_id                   = "subnet-0d9684a07cb886be8"  #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
   associate_public_ip_address = true
-  key_name                    = "moni-key-pair" #Change to your keyname, e.g. jazeel-key-pair
+  key_name                    = "monica-key-pair" #Change to your keyname, e.g. jazeel-key-pair
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
  
   tags = {
@@ -14,7 +14,7 @@ resource "aws_instance" "public" {
 resource "aws_security_group" "allow_ssh" {
   name        = "monica-terraform-security-group" #Security group name, e.g. jazeel-terraform-security-group
   description = "Allow SSH inbound"
-  vpc_id      = "vpc-0e9e0e02f6316c32e"  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+  vpc_id      = "vpc_id = vpc-08fc4fb5b1f5c6227"  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
